9d288dd38d5e49722d4f9a4bf1a996a42a5132e5
xlor1009 <108946864+xlor1009@users.noreply.github.com>
2023-11-13 20:35:28 -0500

    index page

---
+++ app/public/header.php
@@ -0,0 +1,14 @@
+<?php
+	echo"
+    <nav>
+            <ul>
+        <li><a href=\"#\" class=\"secondary\">…</a></li>
+            </ul>
+            <ul>
+        <li><strong>Brand</strong></li>
+           </ul>
+           <ul>
+        <li><a href=\"#\" class=\"secondary\">…</a></li>
+           </ul>
+    </nav> ";
+?>
--- app/public/index.php
+++ app/public/index.php
@@ -1,1 +1,20 @@
-
+<a href="/Users/aminmohammed/Documents/GitHub/CSE3241Final/app/config/style.php"></a>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"
+    />
+    <link rel="stylesheet" href="style.css" />
+    <title>Pico Website</title>
+  </head>
+  <body>
+    <?php include "header.php"; ?>
+    <div class="container">
+      <h1>Articles Worth Reading...</h1>
+    </div>
+  </body>
+</html>
